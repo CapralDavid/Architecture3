d1f00c95815553c04c480460484e462a567bc13a
CapralDavid <ssdrevcukss@gmail.com>
2022-01-16 19:02:40 +0100

    Adding server

---
+++ server/cmd/server/modules.go
@@ -0,0 +1,22 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"github.com/CapralDavid/Architecture3/server/balancers"
+	"github.com/google/wire"
+)
+
+// ComposeApiServer will create an instance of CharApiServer according to providers defined in this file.
+func ComposeApiServer(port HttpPortNumber) (*BalancerApiServer, error) {
+	wire.Build(
+		// DB connection provider (defined in main.go).
+		NewDbConnection,
+		// Add providers from channels package.
+		balancers.Providers,
+		// Provide BalancerApiServer instantiating the structure and injecting channels handler and port number.
+		wire.Struct(new(BalancerApiServer), "Port", "ChannelsHandler"),
+	)
+	return nil, nil
+}
